41242ae5b75be78cbad7f66db6794374fc6483e4
yousufislamme <online.okk@gmail.com>
2023-05-10 16:41:20 +0600

    build a delete option as like github reposigetory

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('hello');
+const textInput = document.getElementById('text-input').addEventListener('keyup', function (event) {
+   const myButton = document.getElementById('myButton');
+   const confirm = event.target.value;
+   if (confirm === 'delete') {
+      myButton.removeAttribute('disabled');
+   } else {
+      myButton.setAttribute('disabled', true);
+   }
+
+})
+
+
+
+document.getElementById('myButton').addEventListener('click', function () {
+   const clearInputText = document.getElementById('text-input');
+   const removeTitle = document.getElementById('title');
+
+   removeTitle.style.display = 'none';
+
+
+   clearInputText.value = '';
+});
