4c0b1669545b01ddc8396d7971afad89da2ade53
yousufislamme <online.okk@gmail.com>
2023-05-10 09:21:22 +0600

    create new branch

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-document.getElementById('post-btn').addEventListener('click', function () {
-   const boxContainer = document.getElementById('box-container');
-   const inputFiled = document.getElementById('input-filed');
-   const p = document.createElement('p');
-
-   p.innerText = inputFiled.value;
-
-   boxContainer.appendChild(p);
-
-   inputFiled.value = '';
-
-});
+console.log('hello');
